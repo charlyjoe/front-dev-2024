a595f9ad5741c3f855bb25224b2e0991160a0220
alexbri22 <alexbri@ciencias.unam.mx>
2024-09-19 18:50:58 -0700

    onInit y html para category component

--- proyecto-frontend/src/app/modules/product/component/category/category.component.ts
+++ proyecto-frontend/src/app/modules/product/component/category/category.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Category } from '../../_model/category';
+import { CategoryService } from '../../_service/category.service';
 
 @Component({
   selector: 'app-category',
   standalone: true,
   imports: [],
   templateUrl: './category.component.html',
-  styleUrl: './category.component.css'
+  styleUrl: './category.component.css',
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit {
+  categories: Category[] = [];
 
+  constructor(private service: CategoryService) {}
+
+  getCategories(): void {
+    this.categories = this.service.getCategories();
+  }
+
+  ngOnInit(): void {
+    this.getCategories();
+  }
 }
