d0853a83f268d40f1fb70262d8dc6013a2c12eb3
alexbri22 <alexbri@ciencias.unam.mx>
2024-09-27 01:02:54 -0600

    modal para agregar una categoria

--- proyecto-frontend/src/app/modules/product/component/category/category.component.ts
+++ proyecto-frontend/src/app/modules/product/component/category/category.component.ts
@@ -1,18 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { Category } from '../../_model/category';
 import { CategoryService } from '../../_service/category.service';
 import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule], // Asegúrate de importar ReactiveFormsModule
   templateUrl: './category.component.html',
-  styleUrl: './category.component.css',
+  styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent implements OnInit {
   categories: Category[] = [];
+  categoryForm: FormGroup;
 
-  constructor(private service: CategoryService) {}
+  constructor(private service: CategoryService, private fb: FormBuilder) {
+    this.categoryForm = this.fb.group({
+      category: ['', Validators.required],
+      tag: ['', Validators.required],
+    });
+  }
 
   getCategories(): void {
     this.categories = this.service.getCategories();
@@ -20,5 +33,18 @@ export class CategoryComponent implements OnInit {
 
   ngOnInit(): void {
     this.getCategories();
+  }
+
+  addCategory(): void {
+    if (this.categoryForm.valid) {
+      const newCategory: Category = {
+        categoryId: this.categories.length + 1,
+        category: this.categoryForm.value.category,
+        tag: this.categoryForm.value.tag,
+        status: 'active',
+      };
+      this.categories.push(newCategory);
+      this.categoryForm.reset();
+    }
   }
 }
